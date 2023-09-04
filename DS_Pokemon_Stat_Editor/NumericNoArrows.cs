b26efdae5644307ca6abb0dc77651967d34f04d9
OtakuGracie <fghorror3@gmail.com>
2023-09-04 13:02:58 -0400

    Added comments to better document code and removed unnccessary method definition

--- DS_Pokemon_Stat_Editor/NumericNoArrows.cs
+++ DS_Pokemon_Stat_Editor/NumericNoArrows.cs
@@ -8,18 +8,13 @@ namespace DS_Pokemon_Stat_Editor
         public NumericNoArrows()
         {
             InitializeComponent();
-            Controls[0].Hide();
-            this.InterceptArrowKeys = false;
-        }
-
-        protected override void OnPaint(PaintEventArgs pe)
-        {
-            base.OnPaint(pe);
+            Controls[0].Hide(); //hides arrows from control
+            InterceptArrowKeys = false;
         }
 
         protected override void OnTextBoxResize(object source, EventArgs e)
         {
-            Controls[1].Width = Width - 4;
+            Controls[1].Width = Width - 4; //resizes the textbox to account for the lack of arrows
         }
         
     }
