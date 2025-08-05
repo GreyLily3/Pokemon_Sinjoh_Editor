d32d49f21f416ce2c3abf2c0afffeed23a9ca082
OtakuGracie <fghorror3@gmail.com>
2025-08-05 12:49:16 -0400

    Updated name for exe files created

--- DS_Pokemon_Stat_Editor/Properties/Resources.Designer.cs
+++ DS_Pokemon_Stat_Editor/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Pokemon_Sinjoh_Editor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DS_Pokemon_Stat_Editor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Pokemon_Sinjoh_Editor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
