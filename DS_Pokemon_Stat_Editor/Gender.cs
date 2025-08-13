26a0759e750de119a2cfef53461aeb3b28f8eb32
OtakuGracie <GreyLily3@proton.me>
2025-08-13 01:23:19 -0400

    created enum for storing Gender of a pokemon

---
+++ DS_Pokemon_Stat_Editor/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Sinjoh_Editor
+{
+    public enum Gender
+    {
+        MALE = 0,
+        FEMALE = 1,
+        UNKNOWN = 2
+    }
+}
