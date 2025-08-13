54f09db9a8e745f44b4ac710afba97c6829f6689
OtakuGracie <GreyLily3@proton.me>
2025-08-13 01:24:23 -0400

    created class for storing personality value of pokemon

---
+++ DS_Pokemon_Stat_Editor/PersonalityValue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon_Sinjoh_Editor
+{
+    public class PersonalityValue
+    {
+        public uint PV;
+
+        public PersonalityValue(uint pv) 
+        {
+            this.PV = pv;
+        }
+
+        public Gender GetGender(byte genderRatio)
+        {
+            if (genderRatio == 255)
+                return Gender.UNKNOWN;
+            else
+                return (PV & 0b_1111_1111) > genderRatio ? Gender.MALE : Gender.FEMALE;
+        }
+
+        public bool HasSecondAbility()
+        {
+            return (PV & 0b_0001) == 0;
+        }
+
+        public uint GetNature()
+        {
+            return PV % 25;
+        }
+    }
+}
