64f09db46bc70cb61e68d894feea5627b7d6b416
GreyLily3 <GreyLily3@proton.me>
2026-06-30 15:37:47 -0400

    Removed unncessary using statements

--- DS_Pokemon_Stat_Editor/TextArchive.cs
+++ DS_Pokemon_Stat_Editor/TextArchive.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Pipes;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+
 
 namespace Pokemon_Sinjoh_Editor
 {
